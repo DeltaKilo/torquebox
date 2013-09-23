6cd8df3accf2078537179fd4566b5d756b983d81
Dmitry Korotkov <codename539@gmail.com>
2013-09-23 05:14:41 +0400

    and couple others

--- support/ci-travis.sh
+++ support/ci-travis.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
+
+function switch_jdk () {
+ sudo update-java-alternatives -s $1
+ JAVA_HOME=$(readlink -f /usr/bin/javac | sed "s:/bin/javac::")
+ echo "Java switched to"
+ java -version
+}
+
+echo "*** Updating enviroment ***"
+sudo apt-get -qq update
+sudo apt-get -qq install openjdk-6-jdk openjdk-7-jdk icedtea-6-plugin icedtea-7-plugin oracle-java7-installer
+switch_jdk java-7-oracle
 
 echo "*** Unsetting variables ***"
 echo "Before:"
@@ -18,6 +29,11 @@ echo "  BUNDLE_PATH: $BUNDLE_PATH"
 echo "*** Environment ***"
 set
 
+echo "*** Memory, CPU & kernel info ***"
+free
+lscpu
+uname -a
+
 echo "*** Setting up Maven repository***"
 mkdir ~/m2repo
 export M2REPO="~/m2repo"
@@ -25,19 +41,17 @@ export M2REPO="~/m2repo"
 echo "*** PWD ***"
 echo $PWD
 
-echo "*** Choosing JDK7 as the build enviroment"
-jdk_switcher use oraclejdk7
-
 echo "*** Starting Build ***"
 
 echo "Performing core build skipping tests"
 mvn -Dmaven.repo.local=$M2_REPO -U -s $TRAVIS_BUILD_DIR/support/settings.xml install -Dmaven.test.skip=true
 
 echo "Performing integ build on JDK7"
+mvn clean -Pinteg
 cd integration-tests && mvn -Dmaven.repo.local=$M2_REPO -U -s $TRAVIS_BUILD_DIR/support/settings.xml test
 
 echo "Performing integ build on JDK6"
 cd $TRAVIS_BUILD_DIR
-jdk_switcher use openjdk6
-mvn clean -Pdist
+switch_jdk java-6-openjdk
+mvn clean -Pinteg
 cd integration-tests && mvn -Dmaven.repo.local=$M2_REPO -U -s $TRAVIS_BUILD_DIR/support/settings.xml test
